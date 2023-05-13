aa5568c503a8e25bf096cdaa24f1a45dcd6dcd60
Your Name <you@example.com>
2023-05-14 06:34:10 +0700

    Добавьте файлы проекта.

---
+++ Laba11/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Laba11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.meanEdit = new System.Windows.Forms.NumericUpDown();
+			this.varianceEdit = new System.Windows.Forms.NumericUpDown();
+			this.sampleEdit = new System.Windows.Forms.NumericUpDown();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.meanEdit)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.varianceEdit)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.sampleEdit)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea2.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea2);
+			legend2.AutoFitMinFontSize = 5;
+			legend2.Enabled = false;
+			legend2.Name = "Legend1";
+			legend2.TextWrapThreshold = 15;
+			this.chart1.Legends.Add(legend2);
+			this.chart1.Location = new System.Drawing.Point(324, 10);
+			this.chart1.Margin = new System.Windows.Forms.Padding(2);
+			this.chart1.Name = "chart1";
+			series2.ChartArea = "ChartArea1";
+			series2.IsValueShownAsLabel = true;
+			series2.Legend = "Legend1";
+			series2.Name = "S";
+			this.chart1.Series.Add(series2);
+			this.chart1.Size = new System.Drawing.Size(692, 346);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(13, 11);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(49, 20);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Mean";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(13, 50);
+			this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(72, 20);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Variance";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(13, 87);
+			this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(95, 20);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Sample size";
+			// 
+			// meanEdit
+			// 
+			this.meanEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.meanEdit.Location = new System.Drawing.Point(228, 6);
+			this.meanEdit.Margin = new System.Windows.Forms.Padding(2);
+			this.meanEdit.Name = "meanEdit";
+			this.meanEdit.Size = new System.Drawing.Size(72, 26);
+			this.meanEdit.TabIndex = 4;
+			this.meanEdit.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+			// 
+			// varianceEdit
+			// 
+			this.varianceEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.varianceEdit.Location = new System.Drawing.Point(228, 44);
+			this.varianceEdit.Margin = new System.Windows.Forms.Padding(2);
+			this.varianceEdit.Name = "varianceEdit";
+			this.varianceEdit.Size = new System.Drawing.Size(72, 26);
+			this.varianceEdit.TabIndex = 5;
+			this.varianceEdit.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+			// 
+			// sampleEdit
+			// 
+			this.sampleEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.sampleEdit.Location = new System.Drawing.Point(228, 81);
+			this.sampleEdit.Margin = new System.Windows.Forms.Padding(2);
+			this.sampleEdit.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+			this.sampleEdit.Name = "sampleEdit";
+			this.sampleEdit.Size = new System.Drawing.Size(72, 26);
+			this.sampleEdit.TabIndex = 6;
+			this.sampleEdit.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(17, 123);
+			this.button1.Margin = new System.Windows.Forms.Padding(2);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(283, 37);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(9, 175);
+			this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(68, 20);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Average";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label5.Location = new System.Drawing.Point(9, 221);
+			this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(72, 20);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Variance";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.Location = new System.Drawing.Point(9, 267);
+			this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(86, 20);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "Chi-square";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1024, 375);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.sampleEdit);
+			this.Controls.Add(this.varianceEdit);
+			this.Controls.Add(this.meanEdit);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.chart1);
+			this.Margin = new System.Windows.Forms.Padding(2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.meanEdit)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.varianceEdit)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.sampleEdit)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown meanEdit;
+        private System.Windows.Forms.NumericUpDown varianceEdit;
+        private System.Windows.Forms.NumericUpDown sampleEdit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Laba11/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            double variance = (double)varianceEdit.Value;
+            double mean = (double)meanEdit.Value;
+            int sample = (int)sampleEdit.Value;
+
+            double[] array = new double[sample];
+
+            for (int i = 0; i < sample; i++)
+            {
+                double BoxMuller = Math.Sqrt(-2 * Math.Log(rand.NextDouble())) * Math.Cos(2 * Math.PI * rand.NextDouble());
+                array[i] = BoxMuller * variance + mean;
+
+            }
+
+            int k = (int)Math.Log(sample, 2) + 1;
+
+            double max = -100;
+            double min = 100;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max) max = array[i];
+                if (array[i] < min) min = array[i];
+                
+            }
+
+            double interval =  (double)(max- min)/(double)k;
+            double[] distribution = new double[k];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    if (array[i] <= min + interval * (j + 1))
+                    {
+                        distribution[j]++;
+                        j = k;
+                    }
+                }
+            }
+
+            double dispersion = 0;
+            double matozhid = 0;
+            double square = 0;
+            double probs = 0;
+
+            for (int i = 0; i < distribution.Length; i++)
+            {
+				matozhid += (double)distribution[i] / (double)sample * i;
+                probs = func(min + (interval/2) * (i + 1), variance, mean);
+                square += (double)(distribution[i] * distribution[i]) / (double)(sample * (double)probs);
+            }
+            square -= sample;
+            square = Math.Round(square, 2);
+
+            chart1.Series[0].Points.Clear();
+
+            for (int i = 0; i < distribution.Length; i++)
+            {
+                dispersion += (double)distribution[i] / (double)(sample) * (i - matozhid) * (i - matozhid);
+                distribution[i] = (double)distribution[i] / (double)sample;
+                chart1.Series[0].Points.AddXY("("+ Math.Round(min + interval * i, 2) + ";"+Math.Round(min + interval * (i+1),2)+"]", distribution[i]);
+            }
+
+            label4.Text = "Average: " + Math.Round(matozhid,2).ToString() + " (error = " + Math.Round(Math.Abs((float)(mean - matozhid)) / mean * 100, 2) + "%)";
+            label5.Text = "Variance: " + Math.Round(dispersion,2).ToString() + " (error = " + Math.Round(Math.Abs((float)(dispersion - variance)) / variance * 100, 2) + "%)";
+            label6.Text = "Chi-squared: " + square + " > 11.07 is ";
+            if (square > 11.07)
+                label6.Text += "true";
+            else label6.Text += "false";
+        }
+
+        private double func(double x, double var, double m)
+        {
+            return 1 / (Math.Sqrt(var) * Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -((x-m)*(x-m))/(2*var));
+        }
+
+    }
+}
